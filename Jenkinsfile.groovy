8db840ea4f186cc70ce03827136033351e4bf42f
Dennis Arslan <dennis.arslan@quby.com>
2019-03-27 10:18:14 +0100

    Jenkinsfile.groovy file extension for pretty syntax highlighting

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    parameters {
+        booleanParam(name: 'cli', defaultValue: true, description: 'Should I rebuild the cli Docker image?')
+        booleanParam(name: 'nginx', defaultValue: true, description: 'Should I rebuild the nginx Docker image?')
+        booleanParam(name: 'php', defaultValue: true, description: 'Should I rebuild the php Docker image?')
+    }
+    stages {
+        stage('Build Image: cli') {
+            steps {
+                sh """
+				docker build -t dennisarslan/denpal-cli -f Dockerfile.cli .
+                docker push dennisarslan/denpal-cli
+                """
+            }
+        }
+        stage('Build Image: nginx') {
+            steps {
+                sh """
+				docker build -t dennisarslan/denpal-nginx -f Dockerfile.nginx .
+                docker push dennisarslan/denpal-nginx
+                """
+            }
+        }
+        stage('Build Image: php') {
+            steps {
+                sh """
+				docker build -t dennisarslan/denpal-php -f Dockerfile.php .
+                docker push dennisarslan/denpal-php
+                """
+            }
+        }
+    }
+}
