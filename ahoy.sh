cc3ba18417aa0b39449f4e5ee6f0fb921b3d2011
Dennis Arslan <dennis.arslan@quby.com>
2019-03-29 11:53:43 +0100

    Fixing typo in ahoy.sh

--- ahoy.sh
+++ ahoy.sh
@@ -3,7 +3,5 @@ docker network prune -f && docker network inspect amazeeio-network >/dev/null ||
 docker-compose up -d --build "$@"
 #docker-compose exec test dockerize -wait tcp://mariadb:3306 -timeout 1m
 #docker-compose exec -T test drush si -y govcms --site-name="Welcome to GovCMS" "$@"
-docker-compose exec -T cli drush status
-echo "status"
-docker-compose ps -a
+#docker-compose exec -T cli drush status
 #docker-compose exec -T cli drush -y sql-sync @master @self
